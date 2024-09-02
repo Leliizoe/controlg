60ccd4c066bacbc889d7b03ac4c06f1a51d4eaef
Lelifresaa7 <peal070705mmcrrsa5@soycecytem.mx>
2024-09-02 12:07:11 -0600

    update

---
+++ ok/control de gastos/app.js
@@ -0,0 +1,159 @@
+var tPresupuesto=parseInt(localStorage.getItem("presupuesto"));
+var gastos=JSON.parse(localStorage.getItem("gastos"))||[];
+var divPresupuesto=document.querySelector('#divPresupuesto');
+var presupuesto=document.querySelector('#presupuesto');
+var btnPresupuesto=document.querySelector('#btnPresupuesto');
+var divGastos=document.querySelector('#divGastos');
+var progress=document.querySelector("#progress");
+var tGastos=0;
+var disponible=0;
+
+const inicio=()=>{
+    tPresupuesto=parseInt(localStorage.getItem("presupuesto"));
+    if(tPresupuesto>0){
+divPresupuesto.classList.remove("d-block")
+divGastos.classList.remove("d-none")
+divPresupuesto.classList.add("d-none")
+divGastos.classList.add("d-block")
+totalPresupuesto.innerHTML=`$ ${tPresupuesto.toFixed(2)}`;
+mostrarGastos();
+}else{
+    divPresupuesto.classList.remove("d-none")
+    divGastos.classList.remove("d-block")
+    divPresupuesto.classList.add("d-block")
+    divGastos.classList.add("d-none") 
+    presupuesto.value=0;
+}
+}
+
+
+
+btnPresupuesto.onclick=()=>{
+    tPresupuesto=parseInt(presupuesto.value);
+    localStorage.setItem('presupuesto',tPresupuesto)
+    if(tPresupuesto==0){
+        Swal.fire({icon: "error", title:"ERROR", text:"Presupuesto mayor a 0"});
+   return;
+    }
+
+    divPresupuesto.classList.remove("d-block");
+    divGastos.classList.remove("d-none");
+    divPresupuesto.classList.add("d-none");
+    divGastos.classList.add("d-block");
+    mostrarGastos();
+    
+}
+const guardarGasto=()=>{
+    gastos=JSON.parse(localStorage.getItem("gastos"))||[];
+    let descripcion=document.getElementById("descripcion").value;
+    let costo=parseInt(document.getElementById("costo").value);
+    let categoria=document.getElementById("categoria").value;
+    if(descripcion.trim()==""||document.getElementById("costo").value.trim()===""||costo==0){
+        Swal.fire({icon:"error", title: "ERROR", text:"DATOS INCORRECTOS"});
+        return;
+    }
+    if(costo>disponible){
+        Swal.fire({icon:"error", title: "ERROR", text:"YA NO TIENES FONDOS"});
+        return;
+    }
+    const gasto={descripcion,costo,categoria}
+    gastos.push(gasto);
+    localStorage.setItem("gastos", JSON.stringify(gastos));
+    bootstrap.Modal.getInstance(document.getElementById("nuevoGasto")).hide();
+    mostrarGastos()
+}
+
+const mostrarGastos=(categoria="todos")=>{
+    gastos=JSON.parse(localStorage.getItem("gastos"))||[];
+    gastosHTML=``;
+    if(gastos.length==0){
+        document.getElementById('listaGastos').innerHTML=`<b>No hay Gastos</b>`
+    }
+    index=0;
+    tGastos=0;
+    gastos.map(gasto=>{
+        if(categoria==gasto.categoria||categoria=="todos"){
+            gastosHTML+=`
+            <div class="card text-center w-100 m-auto mt-3 p-2">
+            <div class="row">
+            <div class="col"><img src="img/${gasto.categoria}.png" class="imgCategoria shadow"></div>
+            <div class="col text-start">
+            <p><b>Descripcion:</b> <small>${parseInt(gasto.costo).toFixed(2)}</small></p>
+            </div>
+        </div>
+    </div> `
+    tGastos+=parseInt(gasto.costo);
+        }
+        index++;
+    })
+    document.getElementById('listaGastos').innerHTML=gastosHTML;
+    pintarDatos()
+}
+
+const pintarDatos=()=>{
+    let totalPresupuesto=document.querySelector("#totalPresupuesto");
+    let totalDisponible=document.querySelector("#totalDisponible");
+    let totalGastos=document.querySelector("#totalGastos");
+    var tPresupuesto=parseInt(localStorage.getItem("presupuesto"));
+    disponible=tPresupuesto-tGastos;
+    let porcentaje=100-((tGastos/tPresupuesto)*100);
+    progress.innerHTML=`<circle-progress value="${porcentaje}" min="0" max="100" text-format="percent"></circle-progress>`
+    totalGastos.innerHTML=`$ ${tGastos.toFixed(2)}`;
+    totalPresupuesto.innerHTML=`$ ${tPresupuesto.toFixed(2)}`;
+    totalDisponible.innerHTML=`$ ${disponible.toFixed(2)}`;
+
+}
+const reset=()=>{
+    localStorage.clear();
+    inicio();
+}
+const actualizarGasto=()=>{
+    gastos=JSON.parse(localStorage.getItem("gastos"))||[];
+
+    let descripcion=document.getElementById("edescripcion").value;
+    let costo=parseInt(document.getElementById("ecosto").value);
+    let categoria=document.getElementById("ecategoria").value;
+    let index=parseInt(document.getElementById("eindex").value);
+    if(descripcion.trim()==""||costo==0){
+        Swal.fire({icon: "error", title:"ERROR", text:"Datos incorrectos"});
+        return;
+
+    }
+    let costoAnterior=parseInt(gastos[index].costo);
+    if(costo>(costoAnterior+disponible)){
+        Swal.fire({icon: "error", title:"ERROR", text:"Ya no tienes fondos"});
+        return;
+    }
+    gastos[index].descripcion=descripcion;
+    gastos[index].costo=costo;
+    gastos[index].categoria=categoria
+    localStorage.setItem("gastos",JSON.stringify(gastos));
+    bootstrap.Modal.getInstance(document.getElementById("editarGasto")).hide();
+    mostrarGastos();
+}
+
+const cargarGasto=(index)=>{
+    var gasto= gastos[index];
+        document.getElementById("edescripcion").value=gasto.descripcion;
+        document.getElementById("ecosto").value=gasto.costo;
+        document.getElementById("ecategoria").value=gasto.categoria;
+        document.getElementById("eindex").value=index;
+}
+
+const deleteGasto=(index)=>{
+    Swal.fire({
+        title:" Esta seguro de Eliminar?",
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: "SI",
+        denyButtonText: `NO`,
+        cancelButtonColor: "#dc3545",
+        confirmButtonColor: "#198754",
+        denyButtonColor: "#dc3545",
+    }).then((result)=>{
+        Swal.fire("Eliminado", "", "success");
+        gastos.splice(index,1)
+        localStorage.setItem("gastos",JSON.stringify(gastos));
+        mostrarGastos()
+    });
+}
